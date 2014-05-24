cbbad298415d1413e860af6147ab49ff20ec2d5e
Adam <electronicbattleweapon7@gmail.com>
2014-05-24 22:26:01 +0100

    super simple example

---
+++ speaker/main.go
@@ -0,0 +1,42 @@
+// Simple introduction to interfaces adapted from
+// http://jordanorelli.com/post/32665860244/how-to-use-interfaces-in-go
+
+package main
+
+import "fmt"
+
+// A "Speaker" is anything that conforms to the Speaker interface, i.e. defines
+// a `Speak` method
+type Speaker interface {
+	Speak() string
+}
+
+// define a couple animals, make them implement the Speak interface
+type Dog struct {
+}
+
+func (d Dog) Speak() string {
+	return "woof!\n"
+}
+
+type Cat struct {
+}
+
+func (c Cat) Speak() string {
+	return "meow!\n"
+}
+
+// This function taks a Speaker, s, as an argument and prefixes it's output
+// from calling `Speak()`
+func animalSays(s Speaker) string {
+	spoke := s.Speak()
+	return "this animal says " + spoke
+}
+
+func main() {
+	speakers := []Speaker{Dog{}, Cat{}}
+	for _, speaker := range speakers {
+		noise := animalSays(speaker)
+		fmt.Println(noise)
+	}
+}
